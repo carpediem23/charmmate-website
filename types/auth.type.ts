8133cc916b0937a57a56cd294ab750b6f92210db
Alptuğ Türkiş <alptug.turkis@outlook.com>
2024-12-25 20:10:54 +0300

    feat: Implement login and registration forms using Formik and Yup for validation

---
+++ types/auth.type.ts
@@ -0,0 +1,10 @@
+export type TLoginFormValues = {
+  email: string;
+  password: string;
+};
+
+export type TRegisterFormValues = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
