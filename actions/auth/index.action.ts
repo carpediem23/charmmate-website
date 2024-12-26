2fafb82863dad8c1c51573e09d528918676cd3f5
Alptuğ Türkiş <alptug.turkis@outlook.com>
2024-12-26 18:04:21 +0300

    feat: Implement secure cookie storage for access token in login action; add cookie serialization and update dependencies

--- actions/auth/index.action.ts
+++ actions/auth/index.action.ts
@@ -3,6 +3,7 @@
 import axios from 'axios';
 import { TLoginFormValues, TLoginResponse } from '@/types/auth.type';
 import { cookies } from 'next/headers';
+import { serialize } from 'cookie';
 
 export async function loginAction(
   formData: TLoginFormValues,
@@ -14,7 +15,15 @@ export async function loginAction(
     );
 
     const cookieStore = await cookies();
-    cookieStore.set('access_token', response.data.access_token);
+    const serializedCookie = serialize('access_token', response.data.access_token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 7 * 4 * 6,
+      path: '/',
+    });
+
+    cookieStore.set('auth', serializedCookie);
 
     return {
       success: true,
