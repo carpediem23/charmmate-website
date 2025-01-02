3ef7cd83fba38de9c37c43e0ae22f5eceb7da4bc
Alptuğ Türkiş <alptug.turkis@outlook.com>
2025-01-02 18:45:25 +0300

    fix: Update environment variable references in build-deploy.yml and remove unused logging in next.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,5 @@ export const metadata = {
 };
 
 export default function HomePage() {
-  console.log(process.env.API_URL);
-  
   return <Home />;
 }
--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig: NextConfig = {
   env: {
     API_URL: process.env.API_URL || 'http://localhost:3000',
   },
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
 };
 
 export default nextConfig;
